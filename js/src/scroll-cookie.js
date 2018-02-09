e6c649bce44abe08223a987f28181ff2ee41f144
AmberCheng <1135122975@qq.com>
2018-02-09 15:18:28 -0500

    Site updated: 2018-02-09 15:18:27

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 09 2018 15:14:27 GMT-0500 (EST)
+// build time:Fri Feb 09 2018 15:17:51 GMT-0500 (EST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
